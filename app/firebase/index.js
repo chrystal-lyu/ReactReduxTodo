7c8d5febe2ecc4444dc8ee4a0fa053cafe5d3347
Crystal Lu <syyssie@gmail.com>
2017-07-12 11:07:18 +0800

    Add actions for firebase login and logout

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -7,11 +7,11 @@ try {
     databaseURL: process.env.DATABASE_URL,
     storageBucket: process.env.STORAGE_BUCKET,
   };
-  console.log(config);
   firebase.initializeApp(config);
 } catch (e) {
 
 }
 
+export var githubProvider = new firebase.auth.GithubAuthProvider();
 export var firebaseRef = firebase.database().ref();
 export default firebase;
