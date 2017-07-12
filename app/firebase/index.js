683c26e56262d9f0965bbaf4b95460443959dfbf
Crystal Lu <syyssie@gmail.com>
2017-07-12 08:56:44 +0800

    Move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,15 +2,14 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyAw0psDEq8KqMRVO3ZIBl9nC3BHpItPcfE",
-    authDomain: "crystal-todo.firebaseapp.com",
-    databaseURL: "https://crystal-todo.firebaseio.com",
-    projectId: "crystal-todo",
-    storageBucket: "crystal-todo.appspot.com",
-    messagingSenderId: "310113746851"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
   };
+  console.log(config);
   firebase.initializeApp(config);
-} catch(e) {
+} catch (e) {
 
 }
 
